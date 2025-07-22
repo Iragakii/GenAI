9fa76a3183272a489861c75550a8f8c03dae6461
iragaki <jcgroup.vni@gmail.com>
2025-07-22 12:23:58 +0700

    figma sidebarchat

--- src/ChatUI/SideBarChat.tsx
+++ src/ChatUI/SideBarChat.tsx
@@ -6,7 +6,9 @@ const homeIconSide = "/icon-home-side.png";
 const brandLogoMain = "/brand-logo-main.png";
 const contactIconSide = "/icon-contact-side.png";
 const aboutIconSide = "/icon-about-side.png";
-
+const agenticIconSide = "/icon-agentic.png";
+const assistantIconSide = "/icon-assistant.png";
+const setupIconSide = "/icon-setup.png";
 const SideBarChat: FC = () => {
   const [showForm, setShowForm] = useState(false);
 
@@ -59,11 +61,11 @@ const SideBarChat: FC = () => {
               <div className="mb-[20px]">
                 <Link
                   to="#"
-                  className="flex items-center justify-center leading-[27px] mb-1 relative box-border rounded-[5px] group bg-[#313131] hover:scale-105 transition-transform duration-300 ease-in-out pt-0 pb-0"
+                  className="flex items-center justify-center leading-[27px] mb-1 relative box-border rounded-[5px] group hover:bg-[#313131] hover:scale-105 transition-transform duration-300 ease-in-out pt-0 pb-0"
                 >
-                  <div className="icon text-[16px] block text-white flex justify-center items-center">
+                  <div className="icon py-1  block text-white flex justify-center items-center">
                     <img
-                      className="line group-hover:scale-[1.2] transition-all duration-300 ease-in-out dark:text-white"
+                      className="w-5  h-5 line group-hover:scale-[1.2] transition-all duration-300 ease-in-out dark:text-white"
                       src={aboutIconSide}
                       alt="iconHome"
                     />
@@ -77,13 +79,13 @@ const SideBarChat: FC = () => {
               <div>
                 <Link
                   to="#"
-                  className="flex items-center py-1 justify-center leading-[27px] mb-1 relative box-border rounded-[5px] group hover:bg-[#313131] hover:scale-105 transition-transform duration-300 ease-in-out"
+                  className="flex items-center py-0 justify-center leading-[27px] mb-1 relative box-border rounded-[5px] group hover:bg-[#313131] hover:scale-105 transition-transform duration-300 ease-in-out"
                 >
                   <div className="icon block text-white flex justify-center items-center">
                     <img
                       src={contactIconSide}
                       alt="logo-company"
-                      className="w-5 h-5 transform transition-transform duration-300 ease-in-out group-hover:scale-[1.2]"
+                      className="w-7 h-7 transform transition-transform duration-300 ease-in-out group-hover:scale-[1.2]"
                     />
                   </div>
                 </Link>
@@ -92,8 +94,61 @@ const SideBarChat: FC = () => {
                 </div>
               </div>
             </div>
-            <div className="text-gray-500 dark:text-gray-400 ">
-              <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-300 w-9 " />
+            <div className="text-gray-500 dark:text-gray-700">
+              <hr className="h-px my-5 bg-[#656262] border-0 w-[calc(100%+15px)] -mx-[8px]" />
+            </div>
+            <div className="mb-[20px] mt-[20px]">
+              <Link
+                to="#"
+                className="flex  items-center justify-center leading-[27px] mb-1 relative box-border rounded-[5px] group hover:bg-[#313131] hover:scale-105 transition-transform duration-300 ease-in-out pt-0 pb-0"
+              >
+                <div className="icon py-1  block text-white flex justify-center items-center">
+                  <img
+                    className="transform w-7 h-5 transition-transform duration-300 ease-in-out group-hover:scale-[1]"
+                    src={agenticIconSide}
+                    alt="iconHome"
+                  />
+                </div>
+              </Link>
+              <div className="text-gray-300 flex items-center justify-center h-full whitespace-nowrap text-[10px]">
+                Agentic
+              </div>
+            </div>
+
+            <div className="mb-[20px]">
+              <Link
+                to="#"
+                className="flex items-center justify-center leading-[27px] mb-1 relative box-border rounded-[5px] group bg-[#313131] hover:scale-105 transition-transform duration-300 ease-in-out pt-0 pb-0"
+              >
+                <div className="icon py-1  block text-white flex justify-center items-center">
+                  <img
+                    className="w-5  h-5 line group-hover:scale-[1.2] transition-all duration-300 ease-in-out dark:text-white"
+                    src={assistantIconSide}
+                    alt="iconHome"
+                  />
+                </div>
+              </Link>
+              <div className="text-gray-300 flex items-center justify-center h-full whitespace-nowrap text-[10px]">
+                List Agent
+              </div>
+            </div>
+
+            <div>
+              <Link
+                to="#"
+                className="flex items-center py-0 justify-center leading-[27px] mb-1 relative box-border rounded-[5px] group hover:bg-[#313131] hover:scale-105 transition-transform duration-300 ease-in-out"
+              >
+                <div className="p-1 icon block text-white flex justify-center items-center">
+                  <img
+                    src={setupIconSide}
+                    alt="logo-company"
+                    className="w-5 h-5 transform transition-transform duration-300 ease-in-out group-hover:scale-[1.2]"
+                  />
+                </div>
+              </Link>
+              <div className="text-gray-300 flex items-center justify-center h-full whitespace-nowrap text-[10px]">
+                Data
+              </div>
             </div>
           </div>
 
